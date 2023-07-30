335adb8a5c9ee01a11433e1a939dd8cd13b3228d
RenatiusvanSander / R. Meier <remad@web.de>
2023-07-30 09:43:05 +0200

    master: a gulp runner, which builds less from src directory and stores them in distro/css

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+var less = require('gulp-less'); 
+var browserSync = require('browser-sync').create();
+var reload      = browserSync.reload;
+
+gulp.task('build-less', function(done) {
+  gulp.src('./src/*.less') .pipe(less()) .pipe(gulp.dest('./distro/css'));
+  done();
+});
